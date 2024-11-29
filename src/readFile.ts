42f0dc60a10db110b4e6cb7b62bb357085dc01dc
Wesley Ferreira <wesleyferreira97@outlook.com>
2024-11-28 23:10:12 -0300

    Initial Commit

---
+++ src/readFile.ts
@@ -0,0 +1,4 @@
+import fs from "node:fs";
+import path from "node:path";
+
+console.log(path.join(__dirname, "db/users.json"));
---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("rota inicial")
+})
+
+app.post("/add-user", (req, res) => {
+    console.log("add user");
+
+})
+
+app.listen(3000)
